d5524ec344a07f4f50cba2c01226a4ac8e685bad
greenbreakfast <lazardemin@gmail.com>
2018-04-09 17:53:03 -0400

    implemented receiving app id from parent, working on better communication with parent

--- Console-App-RFID/src/OnionCDK.js
+++ Console-App-RFID/src/OnionCDK.js
@@ -7,14 +7,26 @@ export default {
   name: 'ConsoleSDK',
   uuid: makeId(),
   makeId: makeId,
-  init: function () {
-    this.sendEvent('Onion.CDK.initialized', {})
+  init: function (callback) {
+    // this.sendEvent('Onion.CDK.initialized', {})
+    // TODO: translate callback to promisse 
+    window.addEventListener('message', function (e) {
+      console.log(e)
+      console.log(this)
+      this.processMessage(e, callback)
+    }.bind(this), false)
   },
   subscribe: function (topic, callback) {
     this.sendEvent('Onion.CDK.subscribe', {topic: topic})
   },
   publish: function (topic, content) {
 
+  },
+  service: function (name, command) {
+    this.sendEvent('Onion.CDK.service', {
+      service: name,
+      command: command
+    })
   },
   sendEvent: function (event, content) {
     // var eventId = this.makeId()
@@ -30,6 +42,13 @@ export default {
   },
   sendCmd: function (command) {
 
+  },
+  processMessage: function (e, callback) {
+    if (e.data.event === 'Onion.CDK.AppInit') {
+      this.appUid = e.data.appUid
+      if (typeof callback === 'function')
+        callback('alldone')
+    }
   }
 
 }
